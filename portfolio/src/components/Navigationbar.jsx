e0b44f44460770a16bfb040742269e168d086cae
Bun <mbunalski@gmail.com>
2023-07-17 14:33:21 -0400

    Logo Modify

--- portfolio/src/components/Navigationbar.jsx
+++ portfolio/src/components/Navigationbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
+import {FaBars, FaTimes}  from 'react-icons/fa'
 
 const Navigationbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-       
+    <div className='fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo Image" style={{width:'100px'}}/>
+        </div>
     </div>
   )
 }
