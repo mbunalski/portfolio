a7dd3a10a0da7213484d690337a30c3d526edbc2
Bun <mbunalski@gmail.com>
2023-07-20 15:31:08 -0400

    mobile menu functinal with hide and see

--- portfolio/src/components/Navigationbar.jsx
+++ portfolio/src/components/Navigationbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/logo.png'
 import {FaBars, FaTimes}  from 'react-icons/fa'
 
 const Navigationbar = () => {
+    const [nav,setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#0a192f] text-[#5cf300]'>
         <div>
@@ -19,12 +21,12 @@ const Navigationbar = () => {
             </ul>
 
         {/*Hamburger*/}
-        <div className='md:hidden z-10'>
-            <FaBars />
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
         </div>
 
         {/*Mobile Menu*/}
-        <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <ul className={!nav ? 'hidden' :'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'>Home</li>
                 <li className='py-6 text-4xl'>About</li>
                 <li className='py-6 text-4xl'>Skills</li>
