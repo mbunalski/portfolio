85cf7a49e111e647a099b7173919dc08bf7c24ac
Bun <mbunalski@gmail.com>
2023-07-21 21:12:10 -0400

    modified home page

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
 
 export const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
         
+        {/*Container*/}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-[#5cf300]'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-gray-100'>Michael Bunalski</h1>
+            <h2 className='text-4xl sm:text-7xl text-gray-400'>I'm a Software Engineer</h2>
+            <p className='text-gray-400 py-4 max-w-[700px]'>I am a full-stack software engineer blah blah blah blah</p>
+            <div>
+                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#5cf300] hover:border-[#5cf300]'>View Work <HiArrowNarrowRight className='ml-2' /> </button>
+            </div>
+        </div>
     </div>
   )
 }
