537df5418765446a0f7e581cb2f82851e38ce907
Bun <mbunalski@gmail.com>
2023-07-24 15:44:26 -0400

    finished skills page

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -11,17 +11,44 @@ import Python from '../assets/python.png'
 
 const Skills = () => {
   return (
-    <div name='skills'>
+    <div name='skills' className=' w-full h-screen bg-[#0a192f]  text-gray-300'>
+
       {/*Container*/}
-      <div className='bg-[#0a192f]'>
+      <div clasName='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full w-full'>
         <div>
-          <p>Experience</p>
-          <p>// These are the techologies I am most proficient with in order</p>
+          <p className='text-4xl border-[#5cf300] font-bold inline border-b-4 ' >Skills</p>
+          <p className=' py-4 '>// These are the techologies I am most proficient with in order</p>
         </div>
 
-        <div>
-          <img src={Python} alt="Python Icon" />
+        <div className ='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto'  src={Python} alt="Python Icon" />
+            <p className='my-4'>Python</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto'  src={Java} alt="Java Icon" />
+            <p className='my-4'>Java</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto'  src={JavaScript} alt="JavaScript Icon" />
+            <p className='my-4'>JavaScript</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto'  src={ReactImg} alt="React Icon" />
+            <p className='my-4'>React</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto'  src={SQL} alt="SQL Icon" />
+            <p className='my-4'>SQL</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto'  src={GitHub} alt="GitHub Icon" />
+            <p className='my-4'>GitHub</p>
+          </div>
+
         </div>
+
       </div>
       
     </div>
