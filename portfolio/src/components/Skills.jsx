531d1091aa18d37f881070f669be05a7c9275776
Bun <mbunalski@gmail.com>
2023-07-24 12:49:44 -0400

    Images added and imports

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import GitHub from '../assets/github.png'
+import SQL from '../assets/sql.png'
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Java from '../assets/java.png'
+import Python from '../assets/python.png'
+
+
+
 const Skills = () => {
   return (
     <div>Skills</div>
