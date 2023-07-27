0d9320a26a2fbcb0fae772db41e87c8dbe5423e8
Bun <mbunalski@gmail.com>
2023-07-27 14:56:59 -0400

    second url added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Navigationbar from "./components/Navigationbar.jsx";
 import Home from "./components/Home.jsx";
@@ -5,18 +7,37 @@ import About from "./components/About.jsx";
 import Skills from "./components/Skills.jsx";
 import Work from "./components/Work.jsx";
 import Contact from "./components/Contact.jsx";
+import ShortUrl from './components/ShortUrl.jsx';
 
-function App() {
-  return (
-    <div>
-      <Navigationbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Navigationbar />
+//       <Home />
+//       <About />
+//       <Skills />
+//       <Work />
+//       <Contact />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          
+            <Routes>
+             <Route path="/" element={<><Navigationbar /><Home /><About /><Skills /><Work /><Contact /></>}/>
+             <Route path="/url" element={<><Navigationbar /><ShortUrl /></>}/>
+             <Route path="/contact" element={<Contact />}/>
+            <Route component={Error}/>
+           </Routes>
+        </div> 
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
