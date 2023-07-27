de19df9b87c089f110f2a69d0a647732cf1fecb3
Bun <mbunalski@gmail.com>
2023-07-26 22:16:20 -0400

    fixed work button

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import {Link} from 'react-scroll'
 
 export const Home = () => {
   return (
@@ -12,11 +13,17 @@ export const Home = () => {
             <h2 className='text-4xl sm:text-7xl text-gray-400'>I'm a Software Engineer</h2>
             <p className='text-gray-400 py-4 max-w-[700px]'>I am a full-stack software engineer and graduate student.</p>
             <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#5cf300] hover:border-[#5cf300]'>View Work 
+                <Link to="work" smooth={true}  duration={500}>
+                    
+                
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#5cf300] hover:border-[#5cf300]'> Work
+                
                 <span className='group-hover:rotate-90 duration-300'>
+                
                 <HiArrowNarrowRight className='ml-2' /> 
                 </span>
                 </button>
+                </Link>
             </div>
         </div>
     </div>
