0c51fbb51053d3abd9c24771d80c2c30669b62af
Bun <mbunalski@gmail.com>
2023-07-28 13:56:55 -0400

    fixing up the URL shortener app

--- portfolio/src/components/ShortUrl.jsx
+++ portfolio/src/components/ShortUrl.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const ShortUrl = () => {
     return (
-
+        
         <div className='w-full h-screen bg-[#0a192f] text-black'>
             <div className='flex flex-col justify-center items-center w-full h-full'>
                 <div className= ' bg bg-pink-200 w-[500px] h-[500px] flex flex-col justify-center items-center'>
+                
                 <Form>
                     <Form.Group className="mb-5" controlId="formBasicEmail">
-                        <Form.Label>Enter a Long URL</Form.Label>
+                        <Form.Label className= 'px-2'>Enter a Long URL</Form.Label>
                         <Form.Control type="email" placeholder="Enter URL"/>
                     </Form.Group>
 
@@ -20,15 +23,17 @@ const ShortUrl = () => {
                     </Button>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Your Short URL</Form.Label>
+                        <Form.Label className= 'px-2' >Your Short URL</Form.Label>
                         <Form.Control type="" placeholder="(eg. 3h6fsB)"/>
                     </Form.Group>
 
                     
                 </Form>
+                
                 </div>
             </div>
         </div>
+        
     );
 }
 
