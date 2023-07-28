007c9f8c5eff2cb144ff6be163f9351676774032
Bun <mbunalski@gmail.com>
2023-07-28 13:14:24 -0400

    short url mods

--- portfolio/src/components/ShortUrl.jsx
+++ portfolio/src/components/ShortUrl.jsx
@@ -2,34 +2,34 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+
 const ShortUrl = () => {
     return (
-        
-        <div className='auth-wrapper'>
-            <div className='auth-wrapper'>
+
+        <div className='w-full h-screen bg-[#0a192f] text-black'>
+            <div className='flex flex-col justify-center items-center w-full h-full'>
+                <div className= ' bg bg-pink-200 w-[500px] h-[500px] flex flex-col justify-center items-center'>
                 <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                    <Form.Group className="mb-5" controlId="formBasicEmail">
+                        <Form.Label>Enter a Long URL</Form.Label>
+                        <Form.Control type="email" placeholder="Enter URL"/>
+                    </Form.Group>
+
+                    <Button className="mb-5 bg-blue-600" variant="primary" type="submit">
+                        Shorten
+                    </Button>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Your Short URL</Form.Label>
+                        <Form.Control type="" placeholder="(eg. 3h6fsB)"/>
+                    </Form.Group>
+
                     
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
                 </Form>
+                </div>
             </div>
         </div>
-      );
+    );
 }
 
 export default ShortUrl
