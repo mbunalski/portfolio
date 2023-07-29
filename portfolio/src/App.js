cd9cf267454d7d5a3a890949d529e05bdd19fac6
Bun <mbunalski@gmail.com>
2023-07-29 19:58:16 -0400

    styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Navigationbar from "./components/Navigationbar.jsx";
 import Home from "./components/Home.jsx";
@@ -10,7 +10,7 @@ import Contact from "./components/Contact.jsx";
 import ShortUrl from './components/ShortUrl.jsx';
 
 // function App() {
-//   return (
+// return (
 //     <div>
 //       <Navigationbar />
 //       <Home />
@@ -19,25 +19,32 @@ import ShortUrl from './components/ShortUrl.jsx';
 //       <Work />
 //       <Contact />
 //     </div>
-//   );
+// );
 // }
 
 class App extends Component {
-  render() {
-    return (      
-       <BrowserRouter>
-        <div>
-          
-            <Routes>
-             <Route path="/" element={<><Navigationbar /><Home /><About /><Skills /><Work /><Contact /></>}/>
-             <Route path="/url" element={<><Navigationbar /><ShortUrl /></>}/>
-             <Route path="/contact" element={<Contact />}/>
-            <Route component={Error}/>
-           </Routes>
-        </div> 
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+
+                    <Routes>
+                        <Route path="/"
+                            element={
+                                <><Navigationbar/><Home/><About/><Skills/><Work/><Contact/></>
+                            }/>
+                        <Route path="/url"
+                            element={
+                                <><Navigationbar/><ShortUrl/></>
+                            }/>
+                        <Route path="/contact"
+                            element={<Contact/>}/>
+                        <Route component={Error}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
