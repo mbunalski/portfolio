911a4069b9c2a6a6ac807e6e9955260547f5d4b5
Bun <mbunalski@gmail.com>
2023-08-01 14:49:39 -0400

    fixing dumb stuff still

--- portfolio/src/utils/firebase.js
+++ portfolio/src/utils/firebase.js
@@ -8,14 +8,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZc0KZDP4RVfFZRE0ufM9yh8vp8OmXEyk",
-  authDomain: "url-shortener-2fdda.firebaseapp.com",
-  projectId: "url-shortener-2fdda",
-  storageBucket: "url-shortener-2fdda.appspot.com",
-  messagingSenderId: "399823372685",
-  appId: "1:399823372685:web:30c6bc12e38425a7bbfdc4",
-  measurementId: "G-226BSJFRFT"
-};
+    apiKey: "AIzaSyD4RQFJsVfLjKTM5rwGMEYrsAUCg6_PUBs",
+    authDomain: "url-app-a191c.firebaseapp.com",
+    projectId: "url-app-a191c",
+    storageBucket: "url-app-a191c.appspot.com",
+    messagingSenderId: "570818146318",
+    appId: "1:570818146318:web:2814638009d6ab1aaa0c5f",
+    measurementId: "G-4HT0WW83Y3"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
