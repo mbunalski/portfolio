276cdbc3229036bdf5d1635880082a90e1248cdb
Bun <mbunalski@gmail.com>
2023-08-01 14:36:28 -0400

    trying to fix dumbshit

--- server/url_app/main.py
+++ server/url_app/main.py
@@ -3,6 +3,6 @@ from firebase_admin import db, credentials
 import os
 
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
