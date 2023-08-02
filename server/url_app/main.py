db83e1e72ceb54f005593ac9568db75419a4be92
Bun <mbunalski@gmail.com>
2023-08-01 22:13:24 -0400

    talking to DB from python

--- server/url_app/main.py
+++ server/url_app/main.py
@@ -4,5 +4,36 @@ import os
 
 
 cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
 
+firebase_admin.initialize_app(cred, {
+    'databaseURL':'https://url-app-a191c-default-rtdb.firebaseio.com/'
+})
+
+ref = db.reference('demo/')
+users_ref = ref.child('users')
+
+users_ref.set({
+    'bobby':{
+        'dob':'June 01, 1912',
+        'sex':'male'},
+    'miley':{
+        'dob':'September 01, 1955',
+        'sex':'female'}
+    })
+
+ref = db.reference('urlapp/')
+
+
+ref.set({
+    'abcdef': 'www.google.com',
+    'f78ghb': 'www.yahoo.com'
+    })
+
+handle = db.reference('urlapp')
+
+snap = ref.get()
+
+if 'abc def' in snap.keys():
+    print('Wammy1')
+else:
+    print('o nooooo')
